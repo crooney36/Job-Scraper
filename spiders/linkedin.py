61bd2126568b5939ebef8eb1562ace255f57c46c
Charles Rooney <crooney36@gmail.com>
2023-08-08 20:24:59 -0400

    beautiful soup scraping

--- spiders/linkedin.py
+++ spiders/linkedin.py
@@ -2,9 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-
 import time
-
 
 def scrape(title, location):
 
